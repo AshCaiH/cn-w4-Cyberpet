f09600752d786e69353cbefb536507c287d94097
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-01-17 13:26:39 +0000

    Added basic animal class

---
+++ main.js
@@ -0,0 +1,10 @@
+class Animal {
+    constructor (name) {
+        this.name = name;
+        this.health = 100;
+        this.happiness = 100;
+        this.fullness = 100;
+        this.notThirstiness = 100;
+        this.energy = 100;
+    }
+}
