0b883435c2469a2c16a2919b3e3e00af75d87cb3
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-01-17 14:58:02 +0000

    Added variables and functions to animals

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 class Animal {
     constructor (name) {
+        this.statuses = ["idle", "sleeping", "eating", "playing"]
+
         this.name = name;
         this.health = 100;
         this.happiness = 100;
         this.fullness = 100;
-        this.notThirstiness = 100;
         this.energy = 100;
         this.description = "Whatever it is, it's definitely an animal.";
+    }
+
+    sleep () {
+        this.energy += 5;
+        this.fullness -= 2;
     }
 }
 
@@ -14,5 +20,7 @@ class Giraffe extends Animal {
     constructor (name) {
         super(name);
         this.description = "This long-necked leaf lover looms loftily."
+        this.eats = "Leaves";
     }
+
 }
