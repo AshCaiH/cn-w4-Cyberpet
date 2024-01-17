219db2d1492b5688714369c2eebf3772ef202f1b
Ash <56586401+AshCaiH@users.noreply.github.com>
2024-01-17 14:29:50 +0000

    Added giraffe class

--- main.js
+++ main.js
@@ -6,5 +6,13 @@ class Animal {
         this.fullness = 100;
         this.notThirstiness = 100;
         this.energy = 100;
+        this.description = "Whatever it is, it's definitely an animal.";
+    }
+}
+
+class Giraffe extends Animal {
+    constructor (name) {
+        super(name);
+        this.description = "This long-necked leaf lover looms loftily."
     }
 }
