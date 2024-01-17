009594fe61e8d0e3aba9495332bb6598ab1aabfb
Andrew Cookson <andrewcookson@email.com>
2024-01-17 15:13:35 +0000

    added feed function

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 class Animal {
-    constructor (name) {
-        this.name = name;
-        this.health = 100;
-        this.happiness = 100;
-        this.fullness = 100;
-        this.notThirstiness = 100;
-        this.energy = 100;
-    }
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.happingitess = 100;
+    this.fullness = 100;
+    this.energy = 100;
+  }
+
+  feed() {
+    this.hunger += 10;
+    this.happiness += 5;
+    this.energy -= 5;
+    this.updateState();
+  }
 }
