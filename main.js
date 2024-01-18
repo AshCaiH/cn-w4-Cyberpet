f86a107a4c1b0cec823b5d1a97471145bc1ced44
Andrew Cookson <andrewcookson@email.com>
2024-01-18 11:41:12 +0000

    updated html, css and javascript

--- main.js
+++ main.js
@@ -15,44 +15,6 @@ class Cat extends Animal {
     this.hunger = 100;
     this.thirst = 100;
   }
-    constructor (name) {
-        this.statuses = ["idle", "sleeping", "eating", "playing"]
-
-        this.name = name;
-        this.health = 100;
-        this.happiness = 100;
-        this.fullness = 100;
-        this.energy = 100;
-        this.description = "Whatever it is, it's definitely an animal.";
-    }
-
-    feed() {
-        this.hunger += 10;
-        this.happiness += 5;
-        this.energy -= 5;
-        this.updateState();
-    }
-  
-    play() {
-        this.happiness += 10;
-        this.energy -= 5;
-        this.hunger -= 5;
-        this.updateState();
-    }
-
-    sleep () {
-        this.energy += 5;
-        this.fullness -= 2;
-    }
-}
-
-class Giraffe extends Animal {
-    constructor (name) {
-        super(name);
-        this.description = "This long-necked leaf lover looms loftily."
-        this.eats = "Leaves";
-    }
-
 }
 
 class Dog extends Animal {
